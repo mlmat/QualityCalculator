7b657108d144715ca9a4c9205d34445ea5b20685
Michal Matuszewski <m.matuszewski.krk@gmail.com>
2019-02-13 22:48:30 +0100

    Deleted one test class

--- app/src/test/java/pl/edu/agh/qualitycalculator/CalculationsTest.java
+++ app/src/test/java/pl/edu/agh/qualitycalculator/CalculationsTest.java
@@ -8,38 +8,54 @@ import static junit.framework.Assert.assertEquals;
 
 public class CalculationsTest {
     Calculations calculationsUnderTest;
-
+    CalculationsVerbalizer calculationsVUnderTest;
     @Before
     public void setUp() {
         calculationsUnderTest = new Calculations();
+
+        calculationsVUnderTest = new CalculationsVerbalizer();
     }
 
     @Test
     public void testSum() throws Exception {
-        assertEquals(5, calculationsUnderTest.calculate(Operation.SUM, 2, 3), 0.01);
+        assertEquals(5, calculationsUnderTest.calculate(Operation.SUM, 2, 3, 0), 0.01);
     }
 
     @Test
     public void testSubtract() throws Exception{
-        assertEquals(13, calculationsUnderTest.calculate(Operation.SUBTRACT, 15, 2), 0.01);
+        assertEquals(13, calculationsUnderTest.calculate(Operation.SUBTRACT, 15, 2, 0), 0.01);
     }
 
     @Test
     public void testDivide() throws Exception {
-        assertEquals(3, calculationsUnderTest.calculate(Operation.DIVIDE, 6, 2), 0.01);
+        assertEquals(3, calculationsUnderTest.calculate(Operation.DIVIDE, 6, 2, 0), 0.01);
     }
 
     @Test
     public void testMultiply () throws Exception {
-        assertEquals(15, calculationsUnderTest.calculate(Operation.MULTIPLY, 3, 5), 0.01);
+        assertEquals(15, calculationsUnderTest.calculate(Operation.MULTIPLY, 3, 5, 0), 0.01);
     }
 
     @Test
     public void testNoData () throws Exception {
-        assertEquals(15, calculationsUnderTest.calculate(Operation.MULTIPLY, 3, 5), 0.01);
+        assertEquals(15, calculationsUnderTest.calculate(Operation.MULTIPLY, 3, 5, 0), 0.01);
     }
+    @Test
+    public void testAvg () throws Exception {
+        assertEquals(10, calculationsUnderTest.calculate(Operation.AVERAGE, 5, 10, 15), 0.01);
+    }
+
+    @Test
+    public void testSumVerbalize () throws Exception {
+        float result = calculationsUnderTest.calculate(Operation.SUM, 1, 1, 0);
+        String verbalization = calculationsVUnderTest.verbalize(Operation.SUM, 1, 1, 0, result);
+
+        assertEquals("1.0 plus 1.0 gives value 2.0", verbalization);
+    }
+
 
     @After
     public void tearDown() {
     }
+
 }
